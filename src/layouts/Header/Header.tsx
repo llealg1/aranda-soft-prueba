f7b6119c93a0b1b8a103c0107ec8a56f74e260e4
lleag1 <luislealgalicia@gmail.com>
2022-05-25 06:53:37 -0400

    init project

---
+++ src/layouts/Header/Header.tsx
@@ -0,0 +1,25 @@
+const Header = () => {
+  return(
+    <div className="headForm">
+      <div className="header">
+        <div className="title">
+          <span className="text1">Recipe<span className="text2">App</span></span>
+        </div>
+        <div className="menuHead">
+          <ul>
+            <li className="selected">Home</li>
+            <li>Vegetarianos</li>
+            <li>Platos Principales</li>
+            <li>Tortas</li>
+            <li>Comida R&aacute;pida</li>
+            <li>Men&uacute; Ni&ntilde;os</li>
+            <li>Sopas</li>
+          </ul>
+        </div>
+        <div className="icoHome"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import Header from '../layouts/Header';
+import '../scss/global.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
